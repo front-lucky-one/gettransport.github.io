dcf7556bcdf5377a7194ccd0620350750c1b1df9
vladik <onvladik7@gmail.com>
2020-01-17 09:08:31 +0200

    Первый коммит

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var burger = document.querySelector('.burger-opener'),
+    nav = document.querySelector('.nav');
+burger.addEventListener('click', function () {
+  return nav.classList.toggle('active');
+});
