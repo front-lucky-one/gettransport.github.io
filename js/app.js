81c5cebe21606f83a1018522006fe139a867b852
vladik <onvladik7@gmail.com>
2020-01-20 21:35:27 +0200

    fin commit

--- js/app.js
+++ js/app.js
@@ -3,5 +3,5 @@
 var burger = document.querySelector('.burger-opener'),
     nav = document.querySelector('.nav');
 burger.addEventListener('click', function () {
-  return nav.classList.toggle('active');
+  return nav.classList.toggle('active-nav');
 });
